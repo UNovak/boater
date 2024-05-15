c3b89a7ac183eb37bb5961bd2c3070d94413c43e
UNovak <88665587+UNovak@users.noreply.github.com>
2024-05-16 00:42:20 +0200

    Adds prettier formating for tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">boater</h1>
+      <div className="scale-125 opacity-50 hover:scale-150 hover:opacity-75">
+        hey
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
