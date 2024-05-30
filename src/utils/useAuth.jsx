df366d9dd660d35f993483564698a635fd474fb5
UNovak <88665587+UNovak@users.noreply.github.com>
2024-05-30 23:56:26 +0200

    Implements writing to global store for login and logout

--- src/utils/useAuth.jsx
+++ src/utils/useAuth.jsx
@@ -1,19 +1,31 @@
 import supabase from "./supabase";
 import { useNavigate } from "react-router-dom";
+import useStore from "./Store";
 
 const useAuth = () => {
   const navigate = useNavigate();
+  const user = {
+    email: 'test@test.com',
+    password: 'password',
+  };
 
   const logIn = async () => {
     let { data, error } = await supabase.auth.signInWithPassword({
-      email: "test@test.com",
-      password: "password",
+      email: user.email,
+      password: user.password,
     });
     if (error) {
       console.error(error);
     } else {
-      console.log("success");
-      console.log(data);
+      const user = {
+        id: data.user.id,
+        email: data.user.email,
+        token: data.session.access_token,
+      }
+      console.log("logged in successfully as: ", data.user);
+      useStore.getState().setUser(user)
+      // TODO:
+      // fetch registration status and redirect accordingly
     }
   };
 
@@ -22,14 +34,16 @@ const useAuth = () => {
     if (error) {
       console.error(error);
     } else {
+      useStore.getState().clearUser();
       navigate("/");
+      console.log("logged out");
     }
   };
 
   const signUp = async () => {
     let { data, error } = await supabase.auth.signUp({
-      email: "test@test.com",
-      password: "password",
+      email: user.email,
+      password: user.password,
     });
     if (error) {
       console.error(error);
