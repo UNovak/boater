0f0525d1c6dc037c59be6cedfc30f85b55889e59
UNovak <88665587+UNovak@users.noreply.github.com>
2024-06-02 05:13:01 +0200

    Implements dropdown logic and adds it to navbar

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
-export const Dropdown = ({ label }) => {
-  const [visible, setVisible] = useState(false);
+export const Dropdown = ({ label, links }) => {
   return (
-    <>
-      {/* trigger button */}
-      <button onClick={() => console.log(label)}>{label}</button>
-      {/* dropdown content */}
-      {visible && <div className="">dropdown</div>}
-    </>
+    <div className="dropdown dropdown-end dropdown-hover">
+      <div tabIndex={0} role="button" className="btn rounded-btn">
+        {label}
+      </div>
+      <div className="absolute z-10 h-7 w-[110%] -translate-x-3 -translate-y-2" />
+      <ul
+        tabIndex={0}
+        className="b menu dropdown-content z-[1] mt-3 w-52 rounded-box bg-base-100 p-2"
+      >
+        {links &&
+          links.map((link, index) => {
+            console.log(link);
+            return (
+              <li key={index} onClick={link.action}>
+                <Link>{link.label}</Link>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,35 @@
 import Dropdown from "@components/Dropdown";
 import Modal from "@components/Modal";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const [modal, setModal] = useState(false);
 
+  const dropdown = [
+    { label: "FAQ", action: () => navigate("/") },
+    { label: "login", action: () => setModal(true) },
+    { label: "signup", action: () => navigate("registration") },
+  ];
+
   return (
-    <header className="navbar bg-cyan-100">
-      <div className="">
-        <NavLink className={"text-4xl text-black "} to={"/"}>
+    <header className=" flex items-center justify-between bg-cyan-100">
+      <div className="flex h-9 items-center justify-between">
+        <NavLink className={"text-4xl text-black"} to={"/"}>
           {/* TODO: logo and title */}
+          logo
         </NavLink>
       </div>
 
       {/* links and dropdown */}
-      <nav>
-        <NavLink className={"btn"} to={"renter"}>
+      <nav className="flex gap-3 *:my-1 *:last:mx-1">
+        <NavLink className={"btn min-h-0"} to={"renter"}>
           I like cash 💰
         </NavLink>
-        <Dropdown label={"lang"} /> {/* dropdown for language */}
-        <Dropdown label={"user"} /> {/* account options dropdown*/}
+        <Modal /> {/* modal for picking language and cirrancy interaction */}
+        {/* account options dropdown*/}
+        <Dropdown label={"hower me"} links={dropdown} />
         <Modal /> {/* modal for login interaction */}
       </nav>
     </header>
