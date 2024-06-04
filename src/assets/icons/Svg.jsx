17d01c7b447f35d266d5185bf901fa40dccc2be9
UNovak <88665587+UNovak@users.noreply.github.com>
2024-06-04 03:18:17 +0200

    "Adds custom svg wrapper"

--- src/assets/icons/Svg.jsx
+++ src/assets/icons/Svg.jsx
@@ -1,8 +1,41 @@
-// import Logo from "./logo.svg?react";
-import Logo from "./logo-full.svg?react";
+import Account from "./account_circle.svg?react";
+import Close from "./close.svg?react";
+import Hidden from "./hidden.svg?react";
+import Logo from "./logo.svg?react";
+import MenuOpen from "./menu_open.svg?react";
+import Trash_closed from "./trashcan_closed.svg?react";
+import Trash_open from "./trashcan_open.svg?react";
+import Person from "./person.svg?react";
+import Visible from "./visible.svg?react";
 
-const Svg = {
+const icon = {
+  Account,
+  Close,
+  Hidden,
   Logo,
+  MenuOpen,
+  Person,
+  Trash_closed,
+  Trash_open,
+  Person,
+  Visible,
 };
 
-export default Svg;
+const Icon = ({ className = "w-6 h-6 content-center", type = "Eye" }) => {
+  const Icon = icon[type];
+
+  // Ensure the selected component exists before rendering
+  if (!Icon) {
+    console.log("error getting svg", type);
+    return null;
+  }
+
+  console.log();
+  return (
+    <div className={className}>
+      <Icon className={"fill-current"} />
+    </div>
+  );
+};
+
+export default Icon;
