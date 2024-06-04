d52668846d4c11b2377ffed9e73c207f870b1871
UNovak <88665587+UNovak@users.noreply.github.com>
2024-06-04 04:18:22 +0200

    Implemented footer and added a few more icons

--- src/assets/icons/Svg.jsx
+++ src/assets/icons/Svg.jsx
@@ -1,32 +1,38 @@
 import Account from "./account_circle.svg?react";
 import Close from "./close.svg?react";
+import Facebook from "./facebook.svg?react";
 import Hidden from "./hidden.svg?react";
 import Logo from "./logo.svg?react";
 import MenuOpen from "./menu_open.svg?react";
+import Person from "./person.svg?react";
 import Trash_closed from "./trashcan_closed.svg?react";
 import Trash_open from "./trashcan_open.svg?react";
-import Person from "./person.svg?react";
+import Twitter from "./twitter.svg?react";
 import Visible from "./visible.svg?react";
+import Youtube from "./youtube.svg?react";
 
 const icon = {
   Account,
   Close,
+  Facebook,
   Hidden,
   Logo,
   MenuOpen,
   Person,
+  Person,
   Trash_closed,
   Trash_open,
-  Person,
+  Twitter,
   Visible,
+  Youtube,
 };
 
-const Icon = ({ className = "w-6 h-6 content-center", type = "Eye" }) => {
+const Icon = ({ className = "w-6 h-6 content-center", type = "" }) => {
   const Icon = icon[type];
 
   // Ensure the selected component exists before rendering
-  if (!Icon) {
-    console.log("error getting svg", type);
+  if (!Icon || type == "") {
+    console.log("error finding an icon", type || undefined);
     return null;
   }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,29 @@
+import Icon from "@icons";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer bg-white p-10 text-neutral-content">
+      <aside>
+        <Icon type="Logo" className="w-20" />
+        <p>Perfect weekend ahead</p>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Socials</h6>
+        <div className="grid grid-flow-col gap-4">
+          <Link to={"#"}>
+            <Icon type="Facebook" className="w-6" />
+          </Link>
+          <Link to={"#"}>
+            <Icon type="Twitter" className="w-6" />
+          </Link>
+          <Link to={"#"}>
+            <Icon type="Youtube" className="w-6" />
+          </Link>
+        </div>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
