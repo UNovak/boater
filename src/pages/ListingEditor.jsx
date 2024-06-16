6be9e13488b36dd2d6f02cdeffb3ece488e64ce2
UNovak <88665587+UNovak@users.noreply.github.com>
2024-06-16 06:44:35 +0200

    Add html for type and location, UI changes

--- src/pages/ListingEditor.jsx
+++ src/pages/ListingEditor.jsx
@@ -48,28 +48,85 @@ export const ListingEditor = (type) => {
             htmlFor="title"
             className="mb-2 block text-sm font-medium text-gray-700 dark:text-white"
           >
-            Boat name
+            Boat name{" "}
+            {errors.title?.type === "required" && (
+              <span role="alert" className="mt-1 text-xs text-red-500">
+                *
+              </span>
+            )}
           </label>
+
           <input
             type="text"
             id="title"
             className="block w-full rounded-lg border border-gray-300  bg-gray-50 p-2 text-sm text-gray-900 shadow-md focus:border-blue-500 focus:shadow-blue-200  focus:outline-none focus:ring-0 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             {...register("title", { required: true })}
-            aria-invalid={errors.firstName ? "true" : "false"}
+            aria-invalid={errors.title ? "true" : "false"}
           />
-          {errors.firstName?.type === "required" && (
-            <p role="alert">First name is required</p>
-          )}
         </div>
+
         <div>
           <label
             htmlFor="fileUpload"
             className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Show off
-          </label>
+          ></label>
           <ImageCollector images={images} setImages={setImages} />
         </div>
+
+        <hr className="mx-auto my-4 h-1 w-48 rounded border-0 bg-gray-100 md:my-10 dark:bg-gray-700" />
+
+        <div className="flex w-full flex-col gap-1 sm:flex-row sm:gap-4">
+          <div>
+            <label
+              htmlFor="type"
+              className="mb-2 inline-block text-sm font-medium text-gray-700 dark:text-white"
+            >
+              Select the type of your boat
+            </label>
+            <select
+              id="countries"
+              placeholder="How would you classify your boat?"
+              className="mb-2 inline w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 sm:mb-0 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            >
+              <option>Sailboat</option>
+              <option>Rubber dinghie</option>
+              <option>Jetski</option>
+              <option>Yacht</option>
+              <option>Catamaran</option>
+              <option>Canoe</option>
+              <option>Other</option>
+            </select>
+          </div>
+          <div className="">
+            <label
+              htmlFor="location"
+              className="mb-2 block text-sm font-medium text-gray-700 dark:text-white"
+            >
+              Where is the boat located
+            </label>
+            <div className="grid w-full grid-cols-8 gap-1">
+              <input
+                type="text"
+                id="Address"
+                placeholder="Koper"
+                className="col-span-4 block w-full rounded-lg border  border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 shadow-md focus:border-blue-500  focus:shadow-blue-200 focus:outline-none focus:ring-0 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+              <input
+                type="text"
+                id="zip"
+                placeholder="6000"
+                className="col-span-2 block w-full rounded-lg border  border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 shadow-md focus:border-blue-500  focus:shadow-blue-200 focus:outline-none focus:ring-0 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+              <input
+                type="text"
+                id="country"
+                placeholder="Slovenia"
+                className="col-span-2 block w-full rounded-lg border  border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 shadow-md focus:border-blue-500 focus:shadow-blue-200 focus:outline-none focus:ring-0 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+            </div>
+          </div>
+        </div>
+
         <div>
           <label
             htmlFor="description"
@@ -88,8 +145,11 @@ export const ListingEditor = (type) => {
           />
         </div>
 
+        <hr className="mx-auto my-2 h-1 w-48 rounded border-0 bg-gray-100 md:my-10 dark:bg-gray-700" />
+
         <input
           type="submit"
+          value="Post listing"
           className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
         />
       </form>
@@ -103,4 +163,4 @@ export default ListingEditor;
 // TODO
 // - Use real form data
 // - Redirect after confirmation
-// - Add more fields
+// - Add more fields: attibutes picker, price per day, space, speed, power
