bdde69144d8e6c10e1d7ab40bdc341858d0b3a7b
UNovak <88665587+UNovak@users.noreply.github.com>
2024-06-19 17:14:28 +0200

    fix: typo

--- src/pages/ListingEditor.jsx
+++ src/pages/ListingEditor.jsx
@@ -89,7 +89,7 @@ export const ListingEditor = (type) => {
             </label>
             <select
               id="type"
-              {...register("type∫∫", {
+              {...register("type", {
                 required: {},
               })}
               className="mb-2 inline w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 sm:mb-0 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
