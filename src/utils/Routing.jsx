a9b554c911d52f643e4e2bcd60a51d013674de20
UNovak <88665587+UNovak@users.noreply.github.com>
2024-07-23 01:47:08 +0200

    refactor: separate route protection
    - separate routes between hosts and users
    - adapt the router to sidebar links and dashbord

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,9 +1,8 @@
-import Host from '@pages/Host'
+import Dashboard from '@pages/Dashboard'
 import LandingPage from '@pages/LandingPage'
 import Listing from '@pages/Listing'
 import ListingEditor from '@pages/ListingEditor'
 import Registration from '@pages/Registration'
-import Renter from '@pages/Renter'
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -15,8 +14,32 @@ import Layout from './Layout'
 import useStore from './Store'
 
 // router for handling the navigation accross the App
-export const ProtectedRoutes = () => {
+export const ProtectedRoutes = ({ condition }) => {
   const authStatus = useStore((state) => state.session.authenticated)
+  const hostRole = useStore((state) => state.user.host)
+
+  console.log('routing protocol: ', authStatus, hostRole)
+
+  if (condition === 'no_host') {
+    if (hostRole) return <Navigate to={'/host'} />
+    return <Outlet />
+  }
+
+  if (condition === 'host' && authStatus) {
+    if (!hostRole) {
+      console.log('access denied need host')
+      return
+    }
+    return <Outlet />
+  }
+
+  if (condition === 'user' && authStatus) {
+    if (hostRole) {
+      console.log('access denied need user')
+      return
+    }
+    return <Outlet />
+  }
 
   return !authStatus ? <Navigate to={'/'} /> : <Outlet />
 }
@@ -24,15 +47,27 @@ export const ProtectedRoutes = () => {
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route index element={<LandingPage />} />
-      <Route path='registration' element={<Registration />} />
-      <Route path='listing/:boat_id' element={<Listing />} />
-      <Route path='host' element={<ProtectedRoutes />}>
-        <Route path=':id' element={<Host />} />
-        <Route path='listing/create' element={<ListingEditor type={'new'} />} />
+      <Route element={<ProtectedRoutes condition='no_host' />}>
+        <Route index element={<LandingPage />} />
+        <Route path='registration' element={<Registration />} />
+        <Route path='listing/:boat_id' element={<Listing />} />
       </Route>
-      <Route path='renter' element={<ProtectedRoutes />}>
-        <Route path='' element={<Renter />} />
+      <Route element={<ProtectedRoutes condition='host' />}>
+        <Route path='host' element={<Dashboard />}>
+          <Route path='settings' element={<Dashboard />} />
+          <Route path='inbox' element={<Dashboard />} />
+        </Route>
+      </Route>
+      <Route element={<ProtectedRoutes condition='user' />}>
+        <Route path='user' element={<Dashboard />}>
+          <Route path='settings' element={<Dashboard />} />
+          <Route path='inbox' element={<Dashboard />} />
+        </Route>
+      </Route>
+      {/* protected routes that handle boats */}
+      <Route element={<ProtectedRoutes />}>
+        <Route path='create' element={<ListingEditor type={'new'} />} />
+        <Route path='edit' element={<ListingEditor type={'edit'} />} />
       </Route>
     </Route>,
   ),
