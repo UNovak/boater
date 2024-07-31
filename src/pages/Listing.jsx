9cfa7f4e03db1d4dec6a5bd86657c3d702039664
UNovak <88665587+UNovak@users.noreply.github.com>
2024-08-01 00:55:02 +0200

    feat: implement fetching all boat data based on URL parameter

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react'
+import useBoats from '@hooks/useBoats'
+import { useParams } from 'react-router-dom'
+
 const Listing = () => {
-  return <h1>Listing</h1>
+  const { getSingleBoat } = useBoats()
+  const { boat_id } = useParams()
+  const [boat, setBoat] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [serverError, setServerError] = useState(null)
+
+  useEffect(() => {
+    setLoading(true)
+    const fetchBoat = async () => {
+      const { data, error } = await getSingleBoat(boat_id)
+      if (error) setServerError(error)
+      if (data) setBoat(data)
+      setLoading(false)
+      return
+    }
+    fetchBoat()
+  }, [])
+
+  const handleReservation = () => {
+    console.log('comming soon')
+  }
+
+  return loading ? (
+    <>Loading...</>
+  ) : (
+    <div className='min-h-lvh w-full'>
+      {serverError && (
+        <div className='text-md m-6 text-red-400'>{serverError.message}</div>
+      )}
+      <main>
+        <div>{JSON.stringify(boat)}</div>
+        <button onClick={() => handleReservation()}>Rent</button>
+      </main>
+    </div>
+  )
 }
 
 export default Listing
