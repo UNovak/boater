54f14b0e65dd03e418ea4f03705cc6c0b9a9ff08
UNovak <88665587+UNovak@users.noreply.github.com>
2024-08-18 02:17:34 +0200

    refactor: remove navbar and footer from registration page

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -5,15 +5,16 @@ import { Outlet, useLocation } from 'react-router-dom'
 
 const Layout = () => {
   const location = useLocation()
+  const isRegistration = location.pathname.startsWith('/registration')
   const isDashboard =
     location.pathname.startsWith('/host') ||
     location.pathname.startsWith('/user')
 
   return (
     <>
-      {!isDashboard && <Navbar />}
+      {!(isDashboard || isRegistration) && <Navbar />}
       <Outlet />
-      {!isDashboard && <Footer />}
+      {!(isDashboard || isRegistration) && <Footer />}
       <Toaster position='bottom-right' reverseOrder={false} />
     </>
   )
