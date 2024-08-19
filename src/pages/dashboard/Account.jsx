065f3db5af47b4d009130370bc16b9b4b082a278
UNovak <88665587+UNovak@users.noreply.github.com>
2024-08-19 20:08:56 +0200

    feat: add temporary contetnt for pages still being worked on

--- src/pages/dashboard/Account.jsx
+++ src/pages/dashboard/Account.jsx
@@ -1,5 +1,14 @@
 const Account = ({ mode }) => {
-  return <>{`${mode} account`}</>
+  return (
+    <>
+      <div className='flex h-screen flex-col items-center justify-center bg-gray-900 text-center'>
+        <div className='text-white'>
+          <h1 className='text-4xl font-bold'>Coming Soon</h1>
+          <p className='mt-4 text-lg'>We're working on something awesome!</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Account
--- src/pages/dashboard/Inbox.jsx
+++ src/pages/dashboard/Inbox.jsx
@@ -1,5 +1,14 @@
 const Inbox = ({ mode }) => {
-  return <>{`${mode} inbox`}</>
+  return (
+    <>
+      <div className='flex h-screen flex-col items-center justify-center bg-gray-900 text-center'>
+        <div className='text-white'>
+          <h1 className='text-4xl font-bold'>Coming Soon</h1>
+          <p className='mt-4 text-lg'>We're working on something awesome!</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Inbox
